2a5e93ab970632e35ef680ecbe15960c0db18e06
LopezDP <davidplopez@live.com>
2019-05-12 14:16:52 -0400

    feat: Adding Routes component with react-router import.

---
+++ src/Routes.js
@@ -0,0 +1,13 @@
+/*
+  Pay Me Now Wallet
+  P2P anonymous payments
+  Routes.js
+*/
+
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./containers/Home";
+
+export default () => <Switch>
+                       <Route path="/" exact component={ Home } />
+                     </Switch>;
