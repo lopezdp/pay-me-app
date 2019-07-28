2003732addee6c8cfe8bf424c04700c0d5aeefd8
LopezDP <davidplopez@live.com>
2019-07-28 14:16:07 -0500

    feat: Adding 404 page not found views.

---
+++ src/containers/404Page.js
@@ -0,0 +1,12 @@
+/*
+  Pay Me Now Wallet
+  P2P anonymous payments
+  404Page.js
+*/
+
+import React from "react";
+import "./404Page.css";
+
+export default () => <div className="Page404">
+                       <h3>Sorry, this page does not exist within the MyPay Platform... yet!</h3>
+                     </div>;
