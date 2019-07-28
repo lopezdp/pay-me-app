37b76aed366d3ce50aaa767ef0d6c4032befb6ed
LopezDP <davidplopez@live.com>
2019-07-28 15:55:48 -0500

    feat: configure and add amplify.

--- src/Routes.js
+++ src/Routes.js
@@ -7,7 +7,11 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import Page404 from "./containers/404Page";
 
 export default () => <Switch>
+                       { /* This is our home page route for the main landing page tyo the app */ }
                        <Route path="/" exact component={ Home } />
+                       { /* This route will catch all unmatched routes && MUST BE LAST!!! */ }
+                       <Route component={ Page404 } />
                      </Switch>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,35 @@ import './index.css';
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
+import Amplify from "aws-amplify";
+import config from "./config";
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  },
+  Storage: {
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID
+  },
+  API: {
+    endpoints: [
+      {
+        // NOTE: The API "name" is critical and used by aws-amplify
+        //       when an amplify API.post() method where the first
+        //       argument is the name of this API field!!!
+        name: "invoice-log-api",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION
+      }
+    ]
+  }
+});
 
 ReactDOM.render(
   <Router>
