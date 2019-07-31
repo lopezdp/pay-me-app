8494f9159e7a98391d713369ab30f0eabbd852c2
LopezDP <davidplopez@live.com>
2019-07-30 19:30:41 -0500

    feat: Added signin route to routes

--- src/Routes.js
+++ src/Routes.js
@@ -7,11 +7,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import Signin from "./containers/Signin";
 import Page404 from "./containers/404Page";
 
 export default () => <Switch>
                        { /* This is our home page route for the main landing page tyo the app */ }
                        <Route path="/" exact component={ Home } />
+                       { /* This is the Login Route */ }
+                       <Route path="/login" exact component={ Signin } />
                        { /* This route will catch all unmatched routes && MUST BE LAST!!! */ }
                        <Route component={ Page404 } />
                      </Switch>
